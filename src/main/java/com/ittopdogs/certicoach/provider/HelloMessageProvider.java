137a33c19d9c6cbf588116676794811adf2385fa
Michiel de Smet <m.desmet@dictu.nl>
2024-10-11 14:40:51 +0200

    #0 - removed everything

--- src/main/java/com/ittopdogs/certicoach/provider/HelloMessageProvider.java
+++
@@ -1,9 +0,0 @@
-package com.ittopdogs.certicoach.provider;
-
-public class HelloMessageProvider {
-
-    public String getMessage(String name) {
-        return "Hello " + name;
-    }
-
-}
--- src/main/java/com/ittopdogs/certicoach/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ittopdogs.certicoach.repository;
-
-import com.ittopdogs.certicoach.model.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepository extends CrudRepository<Message, Long> {
-}
