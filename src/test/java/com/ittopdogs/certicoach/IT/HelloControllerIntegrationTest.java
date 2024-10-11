e54a00b37edb71b417babe23a66b97b9c896e3a5
Michiel de Smet <m.desmet@dictu.nl>
2024-10-11 11:21:22 +0200

    use TestRestTemplate instead of MockMvc

--- src/test/java/com/ittopdogs/certicoach/IT/HelloControllerIntegrationTest.java
+++ src/test/java/com/ittopdogs/certicoach/IT/HelloControllerIntegrationTest.java
@@ -3,36 +3,33 @@ package com.ittopdogs.certicoach.IT;
 import com.ittopdogs.certicoach.provider.HelloMessageProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 class HelloControllerIntegrationTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private TestRestTemplate restTemplate;
 
     @Autowired
     private HelloMessageProvider messageProvider;
 
     @Test
-    void helloEndpointReturnActualMessage() throws Exception {
+    void helloEndpointReturnActualMessage() {
         //Given
         final var name = "Michiel";
         final var expectedMessage = messageProvider.getMessage(name);
 
         //When
-        mockMvc
-            .perform(get("/hello/" + name))
-            .andExpect(status().isOk())
-            .andExpect(content().string(expectedMessage));
+        var result = restTemplate.getForEntity("/hello/Michiel", String.class);
 
+        //Then
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(expectedMessage, result.getBody());
     }
 
 }
