5e966ac8def300656fc03713bd5392b710bb1a29
Michiel de Smet <m.desmet@dictu.nl>
2024-10-11 09:55:58 +0200

    init

---
+++ src/test/java/com/ittopdogs/certicoach/CertiCoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ittopdogs.certicoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertiCoachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
